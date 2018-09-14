a293d89c1bd54692b21bd938b8b7906c6b4a6ac3
And42 <andrewskobcov@yandex.ru>
2018-09-14 22:41:46 +0300

    Some changes

---
+++ SettingsManager/ISettingsContainer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace SettingsManager
+{
+    public interface ISettingsContainer : INotifyPropertyChanged
+    {
+        bool AutoFlush { get; set; }
+
+        T GetValue<T>(string settingName);
+
+        void SetValue<T>(string settingName, T value);
+
+        object GetValue(string settingName);
+
+        void SetValue(string settingName, object value);
+
+        void Save();
+    }
+}
